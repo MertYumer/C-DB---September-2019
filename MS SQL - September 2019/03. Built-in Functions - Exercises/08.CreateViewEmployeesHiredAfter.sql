1aeb9f5bae9dbeb0dc9e5274b0a94ff702aa2f0a
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-09-24 16:58:52 +0300

    Refactored some queries

--- MS SQL - September 2019/03. Built-in Functions - Exercises/08.CreateViewEmployeesHiredAfter.sql
+++ MS SQL - September 2019/03. Built-in Functions - Exercises/08.CreateViewEmployeesHiredAfter.sql
@@ -1,5 +1,5 @@
 CREATE VIEW V_EmployeesHiredAfter2000 AS
-	 SELECT e.FirstName,
-			e.LastName
-	   FROM Employees AS e
-	  WHERE YEAR(e.HireDate) > 2000
+SELECT e.FirstName,
+	   e.LastName
+  FROM Employees AS e
+ WHERE YEAR(e.HireDate) > 2000
--- MS SQL - September 2019/03. Built-in Functions - Exercises/16.GetUsersWithIPAddressLikePattern.sql
+++ MS SQL - September 2019/03. Built-in Functions - Exercises/16.GetUsersWithIPAddressLikePattern.sql
@@ -1,5 +1,5 @@
   SELECT u.Username,
          u.IpAddress AS [IP Address] 
     FROM Users AS u
-   WHERE U.IpAddress LIKE '___.1%.%.___'
+   WHERE u.IpAddress LIKE '___.1%.%.___'
 ORDER BY u.Username
