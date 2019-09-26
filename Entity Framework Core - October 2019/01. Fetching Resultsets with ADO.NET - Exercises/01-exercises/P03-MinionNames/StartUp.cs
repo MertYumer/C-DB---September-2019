c70763c112f6e7e56ab473ae6cbf1f9462fefc15
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-09-26 15:59:28 +0300

    Added new exercises

---
+++ Entity Framework Core - October 2019/01. Fetching Resultsets with ADO.NET - Exercises/01-exercises/P03-MinionNames/StartUp.cs
@@ -0,0 +1,69 @@
+﻿namespace P03_MinionNames
+{
+    using P01_InitialSetup;
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.Write("Enter Villain ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string villainNameQuery = @"SELECT Name 
+                                              FROM Villains 
+                                             WHERE Id = @id";
+
+                using (SqlCommand command = new SqlCommand(villainNameQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    string villainName = (string)command.ExecuteScalar();
+
+                    if (villainName == null)
+                    {
+                        Console.WriteLine($"No villain with ID {id} exists in the database.");
+                        return;
+                    }
+
+                    Console.WriteLine($"Villain: {villainName}");
+                }
+
+                string minionsQuery = @"  SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                                 m.Name, 
+                                                 m.Age
+                                            FROM MinionsVillains AS mv
+                                            JOIN Minions As m ON mv.MinionId = m.Id
+                                           WHERE mv.VillainId = @id
+                                        ORDER BY m.Name";
+
+                using (SqlCommand command = new SqlCommand(minionsQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("(no minions)");
+                            return;
+                        }
+
+                        while (reader.Read())
+                        {
+                            long rowNumber = (long)reader["RowNum"];
+                            string name = (string)reader["Name"];
+                            int minionsCount = (int)reader["Age"];
+
+                            Console.WriteLine($"{rowNumber}. {name} {minionsCount}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
