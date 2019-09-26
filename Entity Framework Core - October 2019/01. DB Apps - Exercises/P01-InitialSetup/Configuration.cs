4292584655adab8ae683b57126d2eab75049cbf4
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-09-26 16:09:38 +0300

    Updated 01. DB Apps - Exercises

---
+++ Entity Framework Core - October 2019/01. DB Apps - Exercises/P01-InitialSetup/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_InitialSetup
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = "Server=DESKTOP-BK4DHUV" +
+            "\\SQLEXPRESS;Database=MinionDB;Integrated Security=True";
+    }
+}
--- Entity Framework Core - October 2019/01. Fetching Resultsets with ADO.NET - Exercises/01-exercises/P01-InitialSetup/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P01_InitialSetup
-{
-    public static class Configuration
-    {
-        public const string ConnectionString = "Server=DESKTOP-BK4DHUV" +
-            "\\SQLEXPRESS;Database=MinionDB;Integrated Security=True";
-    }
-}
