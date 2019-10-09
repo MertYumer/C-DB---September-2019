5be47285cad80657bce299f63f86a3113093612d
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-10-09 20:07:10 +0300

    Added new exercises

---
+++ Entity Framework Core - October 2019/03. EntityFramework Introduction - Exercises/P06-AddingAndUpdating/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = AddNewAddressToEmployee(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            var employee = context.Employees
+                .FirstOrDefault(x => x.LastName == "Nakov");
+
+            employee.Address = address;
+            context.SaveChanges();
+
+            var addresses = context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address.AddressText);
+
+            foreach (var currentAddress in addresses)
+            {
+                stringBuilder.AppendLine(currentAddress);
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Entity Framework Core - October 2019/03. EntityFramework Introduction - Exercises/P07-EmployeesAndProjects/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetEmployeesInPeriod(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.EmployeesProjects
+                    .Any(p => p.Project.StartDate.Year >= 2001 && p.Project.StartDate.Year <= 2003))
+                .Take(10)
+                .Select(e => new
+                {
+                    FullName = e.FirstName + " " + e.LastName,
+                    ManagerFullName = e.Manager.FirstName + " " + e.Manager.LastName,
+                    Projects = e.EmployeesProjects
+                    .Select(p => new
+                    {
+                        p.Project.Name,
+                        p.Project.StartDate,
+                        p.Project.EndDate
+                    })
+                });
+
+            foreach (var employee in employees)
+            {
+                stringBuilder.AppendLine($"{employee.FullName} - Manager: {employee.ManagerFullName}");
+
+                foreach (var project in employee.Projects)
+                {
+                    string format = "M/d/yyyy h:mm:ss tt";
+                    string startDate = project.StartDate
+                        .ToString(format, CultureInfo.InvariantCulture);
+
+                    string endDate = project.EndDate != null 
+                        ? project.EndDate.Value.ToString(format, CultureInfo.InvariantCulture) 
+                        : "not finished";
+
+                    stringBuilder.AppendLine($"--{project.Name} - {startDate} - {endDate}");
+                }
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
