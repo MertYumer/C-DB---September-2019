e634d25a8d3ca496ea5bbceaca45857768366e86
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-10-30 18:55:44 +0200

    Added new exercises

---
+++ Entity Framework Core - October 2019/06. Advanced Querying - Exercises/BookShop/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace BookShop
+{
+    using Models.Enums;
+    using Data;
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                var result = GetGoldenBooks(db);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true);
+
+            var bookTitles = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            var result = String.Join(Environment.NewLine, bookTitles);
+
+            return result;
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var bookTitles = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title);
+
+            var result = String.Join(Environment.NewLine, bookTitles);
+
+            return result;
+        }
+    }
+}
