a0804ba39b3bbb25f25af29807bf641386ab1144
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-11-12 15:53:29 +0200

    Added new exercises

--- Entity Framework Core - October 2019/09. XML Processing/ProductShop/ProductShopProfile.cs
+++ Entity Framework Core - October 2019/09. XML Processing/ProductShop/ProductShopProfile.cs
@@ -3,6 +3,8 @@
     using AutoMapper;
     using Dtos.Import;
     using Models;
+    using Dtos.Export;
+    using System.Linq;
 
     public class ProductShopProfile : Profile
     {
@@ -15,6 +17,18 @@
             CreateMap<ImportCategoryDto, Category>();
 
             CreateMap<ImportCategoryProductsDto, CategoryProduct>();
+
+            CreateMap<Product, ProductInRangeDto>()
+                .ForMember(dest => dest.Buyer, 
+                opt => opt.MapFrom(src => $"{src.Buyer.FirstName} {src.Buyer.LastName}"));
+
+            CreateMap<Category, CategoryWithProductsDto>()
+                .ForMember(dest => dest.Count,
+                opt => opt.MapFrom(src => src.CategoryProducts.Count))
+                .ForMember(dest => dest.AveragePrice,
+                opt => opt.MapFrom(src => src.CategoryProducts.Average(cp => cp.Product.Price)))
+                .ForMember(dest => dest.TotalRevenue,
+                opt => opt.MapFrom(src => src.CategoryProducts.Sum(cp => cp.Product.Price)));
         }
     }
 }
