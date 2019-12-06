d0276320287f1659edd6cbd6b908d27ffc7cdaa2
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-12-06 22:59:28 +0200

    Added exam preparation

---
+++ Entity Framework Core - October 2019/Exam Preparation/Cinema/CinemaProfile.cs
@@ -0,0 +1,37 @@
+﻿namespace Cinema
+{
+    using System;
+    using System.Linq;
+
+    using AutoMapper;
+    using Cinema.Data.Models;
+    using Cinema.Data.Models.Enums;
+    using Cinema.DataProcessor.ExportDto;
+    using Cinema.DataProcessor.ImportDto;
+
+    public class CinemaProfile : Profile
+    {
+        public CinemaProfile()
+        {
+            this.CreateMap<ImportMovieDto, Movie>()
+                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => (Genre)Enum.Parse(typeof(Genre), src.Genre)))
+                .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => TimeSpan.Parse(src.Duration)));
+
+            this.CreateMap<ImportHallWithSeatsDto, Hall>();
+
+            this.CreateMap<ImportProjectionDto, Projection>()
+                .ForMember(dest => dest.DateTime, opt => opt.MapFrom(src => DateTime.Parse(src.DateTime)));
+
+            this.CreateMap<ImportTicketDto, Ticket>();
+
+            this.CreateMap<ImportCustomerWithTicketsDto, Customer>();
+
+            this.CreateMap<Customer, ExportCustomerWithSpentMoneyDto>()
+                .ForMember(dest => dest.SpentMoney, 
+                opt => opt.MapFrom(src => src.Tickets.Sum(t => t.Price).ToString("f2")))
+                .ForMember(dest => dest.SpentTime, 
+                opt => opt.MapFrom(src => TimeSpan.FromMilliseconds(
+                    src.Tickets.Sum(t => t.Projection.Movie.Duration.TotalMilliseconds)).ToString(@"hh\:mm\:ss")));
+        }
+    }
+}
---
+++ Entity Framework Core - October 2019/Exam Preparation/Cinema/DataProcessor/Serializer.cs
@@ -0,0 +1,75 @@
+﻿namespace Cinema.DataProcessor
+{
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    using AutoMapper;
+    using Cinema.DataProcessor.ExportDto;
+    using Data;
+    using Newtonsoft.Json;
+
+    public class Serializer
+    {
+        public static string ExportTopMovies(CinemaContext context, int rating)
+        {
+            var movies = context
+                .Movies
+                .Where(m => m.Rating >= rating
+                    && m.Projections.Any(p => p.Tickets.Any()))
+                .OrderByDescending(m => m.Rating)
+                .ThenByDescending(m => m.Projections.Sum(p => p.Tickets.Sum(t => t.Price)))
+                .Take(10)
+                .ToArray();
+
+            var movieDtos = movies
+                .Select(m => new ExportMovieDto
+                {
+                    MovieName = m.Title,
+                    Rating = m.Rating.ToString("f2"),
+                    TotalIncomes = m.Projections.Sum(p => p.Tickets.Sum(t => t.Price)).ToString("f2"),
+
+                    Customers = m.Projections.SelectMany(p => p.Tickets)
+                    .Select(t => new ExportCustomerDto
+                    {
+                        FirstName = t.Customer.FirstName,
+                        LastName = t.Customer.LastName,
+                        Balance = t.Customer.Balance.ToString("f2")
+                    })
+                    .OrderByDescending(c => c.Balance)
+                    .ThenBy(c => c.FirstName)
+                    .ThenBy(c => c.LastName)
+                    .ToArray()
+                })
+                .ToArray();
+
+            var jsonResult = JsonConvert.SerializeObject(movieDtos, Newtonsoft.Json.Formatting.Indented);
+
+            return jsonResult;
+        }
+
+        public static string ExportTopCustomers(CinemaContext context, int age)
+        {
+            var customers = context
+                .Customers
+                .Where(c => c.Age >= age)
+                .OrderByDescending(c => c.Tickets.Sum(t => t.Price))
+                .Take(10)
+                .ToArray();
+
+            var customerDtos = Mapper.Map<ExportCustomerWithSpentMoneyDto[]>(customers);
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportCustomerWithSpentMoneyDto[]),
+                           new XmlRootAttribute("Customers"));
+
+            var stringBuilder = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+            xmlSerializer.Serialize(new StringWriter(stringBuilder), customerDtos, namespaces);
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
